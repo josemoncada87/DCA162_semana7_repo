72eb28b4fe71aa1d32c91cd46df336c984318bcd
JoseD <JoseD@172.30.169.206>
2016-09-16 14:36:08 -0500

    una variable

--- DCA162EjercicioSemana7/src/principalEjemplo.java
+++ DCA162EjercicioSemana7/src/principalEjemplo.java
@@ -3,21 +3,11 @@ public class principalEjemplo {
 
 	public static void main(String[] args) {
 		System.out.println("hallo");
-		
+
 		/**
 		 * me encanta la clase algoritmos.... <3
-		 * */
-<<<<<<< HEAD
-		
-		/**
-		 * help :'v
 		 */
-
-=======
-		/*
-		 * Perderé algoritmos, aiudaaaa ._.
-		 */
->>>>>>> branch 'master' of https://github.com/josemoncada87/DCA162_semana7_repo.git
+		int slvskndlvl;
 	}
 
 }
