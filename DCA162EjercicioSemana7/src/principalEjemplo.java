8d9b8327126c96963ec39c5926bbcfea854a3942
Julio César <Julio Csar@172.30.181.123>
2016-09-16 14:33:38 -0500

    borre mensaje

--- DCA162EjercicioSemana7/src/principalEjemplo.java
+++ DCA162EjercicioSemana7/src/principalEjemplo.java
@@ -4,9 +4,7 @@ public class principalEjemplo {
 	public static void main(String[] args) {
 		System.out.println("hallo");
 		
-		/**
-		 * me encanta la clase algoritmos.... <3
-		 * */
+		
 		/*
 		 * Perderé algoritmos, aiudaaaa ._.
 		 */
