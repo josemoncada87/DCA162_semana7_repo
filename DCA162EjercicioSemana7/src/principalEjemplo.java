0604a99013cf2ffa34efe426a37260919af9f9e6
JoseD <JoseD@172.30.169.206>
2016-09-16 14:41:10 -0500

    (._.

--- DCA162EjercicioSemana7/src/principalEjemplo.java
+++ DCA162EjercicioSemana7/src/principalEjemplo.java
@@ -3,18 +3,18 @@ public class principalEjemplo {
 
 	public static void main(String[] args) {
 		System.out.println("hallo");
-<<<<<<< HEAD
+
 
 		/**
 		 * me encanta la clase algoritmos.... <3
-=======
+
 		
 		
 		/*
 		 * Perderé algoritmos, aiudaaaa ._.
->>>>>>> branch 'master' of https://github.com/josemoncada87/DCA162_semana7_repo.git
+
 		 */
-		int slvskndlvl;
+		int slvskndlvls;
 	}
 
 }
